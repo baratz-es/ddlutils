5fee982b84edede3f99df102bd61fb50230b2fa6
Luis Panadero Guardeño <luis.panadero@digibis.com>
2025-03-25 17:07:27 +0100

    UT-27 Fix bueno del manejo de los VARCHAR, NVARCHAR, TEXT, NTEXT ... en SqlServer

--- src/main/java/org/apache/ddlutils/model/TypeMap.java
+++ src/main/java/org/apache/ddlutils/model/TypeMap.java
@@ -2,13 +2,13 @@ package org.apache.ddlutils.model;
 
 /*
  * Copyright 1999-2006 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,9 +19,11 @@ package org.apache.ddlutils.model;
 import java.sql.Types;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.ddlutils.util.Jdbc3Utils;
+import org.apache.ddlutils.util.Jdbc4Utils;
 
 /**
  * A class that maps SQL type names to their JDBC type ID found in
@@ -95,12 +97,21 @@ public abstract class TypeMap
     /** The string representation of the {@link java.sql.Types#VARCHAR} constant. */
     public static final String VARCHAR       = "VARCHAR";
 
+    /** The string representation of the {@link java.sql.Types#NVARCHAR} constant. */
+    public static final String NVARCHAR       = "NVARCHAR";
+    /** The string representation of the {@link java.sql.Types#NCHAR} constant. */
+    public static final String NCHAR          = "NCHAR";
+    /** The string representation of the {@link java.sql.Types#LONGNVARCHAR} constant. */
+    public static final String LONGNVARCHAR   = "LONGNVARCHAR";
+    /** The string representation of the {@link java.sql.Types#NCLOB} constant. */
+    public static final String NCLOB          = "NCLOB";
+
     /** Maps type names to the corresponding {@link java.sql.Types} constants. */
-    private static HashMap _typeNameToTypeCode = new HashMap();
+    private static Map<String, Integer> _typeNameToTypeCode = new HashMap<>();
     /** Maps {@link java.sql.Types} type code constants to the corresponding type names. */
-    private static HashMap _typeCodeToTypeName = new HashMap();
+    private static Map<Integer, String> _typeCodeToTypeName = new HashMap<>();
     /** Conatins the types per category. */
-    private static HashMap _typesPerCategory = new HashMap();
+    private static Map<JdbcTypeCategoryEnum, Set<Integer>> _typesPerCategory = new HashMap<>();
 
     static
     {
@@ -139,56 +150,75 @@ public abstract class TypeMap
             registerJdbcType(Jdbc3Utils.determineBooleanTypeCode(),  BOOLEAN,  JdbcTypeCategoryEnum.NUMERIC);
             registerJdbcType(Jdbc3Utils.determineDatalinkTypeCode(), DATALINK, JdbcTypeCategoryEnum.SPECIAL);
         }
+        if (Jdbc4Utils.supportsJdbc40Types()) {
+            try {
+                registerJdbcType(Jdbc4Utils.determineNCharTypeCode(), NCHAR, JdbcTypeCategoryEnum.TEXTUAL);
+            } catch (UnsupportedOperationException ex) {
+            }
+            try {
+                registerJdbcType(Jdbc4Utils.determineNVarcharTypeCode(), NVARCHAR, JdbcTypeCategoryEnum.TEXTUAL);
+            } catch (UnsupportedOperationException ex) {
+            }
+            try {
+                registerJdbcType(Jdbc4Utils.determineLongNVarcharTypeCode(), LONGNVARCHAR,
+                    JdbcTypeCategoryEnum.TEXTUAL);
+            } catch (UnsupportedOperationException ex) {
+            }
+            try {
+                registerJdbcType(Jdbc4Utils.determineNClobTypeCode(), NCLOB, JdbcTypeCategoryEnum.TEXTUAL);
+            } catch (UnsupportedOperationException ex) {
+            }
+        }
 
         // Torque/Turbine extensions which we only support when reading from an XML schema
-        _typeNameToTypeCode.put("BOOLEANINT",  new Integer(Types.TINYINT));
-        _typeNameToTypeCode.put("BOOLEANCHAR", new Integer(Types.CHAR));
+        _typeNameToTypeCode.put("BOOLEANINT",  Types.TINYINT);
+        _typeNameToTypeCode.put("BOOLEANCHAR", Types.CHAR);
     }
 
     /**
      * Returns the JDBC type code (one of the {@link java.sql.Types} constants) that
      * corresponds to the given JDBC type name.
-     * 
+     *
      * @param typeName The JDBC type name (case is ignored)
      * @return The type code or <code>null</code> if the type is unknown
      */
     public static Integer getJdbcTypeCode(String typeName)
     {
-        return (Integer)_typeNameToTypeCode.get(typeName.toUpperCase());
+        return _typeNameToTypeCode.get(typeName.toUpperCase());
     }
 
     /**
      * Returns the JDBC type name that corresponds to the given type code
      * (one of the {@link java.sql.Types} constants).
-     * 
+     *
      * @param typeCode The type code
      * @return The JDBC type name (one of the constants in this class) or
      *         <code>null</code> if the type is unknown
      */
     public static String getJdbcTypeName(int typeCode)
     {
-        return (String)_typeCodeToTypeName.get(new Integer(typeCode));
+        return _typeCodeToTypeName.get(Integer.valueOf(typeCode));
     }
 
     /**
      * Registers a jdbc type.
-     * 
+     *
      * @param typeCode The type code (one of the {@link java.sql.Types} constants)
      * @param typeName The type name (case is ignored)
      * @param category The type category
      */
-    protected static void registerJdbcType(int typeCode, String typeName, JdbcTypeCategoryEnum category) 
+    protected static void registerJdbcType(int typeCode, String typeName, JdbcTypeCategoryEnum category)
     {
-        Integer typeId = new Integer(typeCode);
+        Integer typeId = typeCode;
 
         _typeNameToTypeCode.put(typeName.toUpperCase(), typeId);
         _typeCodeToTypeName.put(typeId, typeName.toUpperCase());
 
-        Set typesInCategory = (Set)_typesPerCategory.get(category);
+        Set<Integer> typesInCategory = _typesPerCategory.get(category);
 
         if (typesInCategory == null)
         {
-            typesInCategory = new HashSet();
+            typesInCategory = new HashSet<>();
             _typesPerCategory.put(category, typesInCategory);
         }
         typesInCategory.add(typeId);
@@ -197,70 +227,70 @@ public abstract class TypeMap
     /**
      * Determines whether the given jdbc type (one of the {@link java.sql.Types} constants)
      * is a numeric type.
-     * 
+     *
      * @param jdbcTypeCode The type code
      * @return <code>true</code> if the type is a numeric one
      */
     public static boolean isNumericType(int jdbcTypeCode)
     {
-        Set typesInCategory = (Set)_typesPerCategory.get(JdbcTypeCategoryEnum.NUMERIC);
+        Set<Integer> typesInCategory = _typesPerCategory.get(JdbcTypeCategoryEnum.NUMERIC);
 
-        return typesInCategory == null ? false : typesInCategory.contains(new Integer(jdbcTypeCode));
+        return typesInCategory == null ? false : typesInCategory.contains(Integer.valueOf(jdbcTypeCode));
     }
 
     /**
      * Determines whether the given jdbc type (one of the {@link java.sql.Types} constants)
      * is a date/time type.
-     * 
+     *
      * @param jdbcTypeCode The type code
      * @return <code>true</code> if the type is a numeric one
      */
     public static boolean isDateTimeType(int jdbcTypeCode)
     {
-        Set typesInCategory = (Set)_typesPerCategory.get(JdbcTypeCategoryEnum.DATETIME);
+        Set<Integer> typesInCategory = _typesPerCategory.get(JdbcTypeCategoryEnum.DATETIME);
 
-        return typesInCategory == null ? false : typesInCategory.contains(new Integer(jdbcTypeCode));
+        return typesInCategory == null ? false : typesInCategory.contains(Integer.valueOf(jdbcTypeCode));
     }
 
     /**
      * Determines whether the given jdbc type (one of the {@link java.sql.Types} constants)
      * is a text type.
-     * 
+     *
      * @param jdbcTypeCode The type code
      * @return <code>true</code> if the type is a text one
      */
     public static boolean isTextType(int jdbcTypeCode)
     {
-        Set typesInCategory = (Set)_typesPerCategory.get(JdbcTypeCategoryEnum.TEXTUAL);
+        Set<Integer> typesInCategory = _typesPerCategory.get(JdbcTypeCategoryEnum.TEXTUAL);
 
-        return typesInCategory == null ? false : typesInCategory.contains(new Integer(jdbcTypeCode));
+        return typesInCategory == null ? false : typesInCategory.contains(Integer.valueOf(jdbcTypeCode));
     }
 
     /**
      * Determines whether the given jdbc type (one of the {@link java.sql.Types} constants)
      * is a binary type.
-     * 
+     *
      * @param jdbcTypeCode The type code
      * @return <code>true</code> if the type is a binary one
      */
     public static boolean isBinaryType(int jdbcTypeCode)
     {
-        Set typesInCategory = (Set)_typesPerCategory.get(JdbcTypeCategoryEnum.BINARY);
+        Set<Integer> typesInCategory = _typesPerCategory.get(JdbcTypeCategoryEnum.BINARY);
 
-        return typesInCategory == null ? false : typesInCategory.contains(new Integer(jdbcTypeCode));
+        return typesInCategory == null ? false : typesInCategory.contains(Integer.valueOf(jdbcTypeCode));
     }
 
     /**
      * Determines whether the given sql type (one of the {@link java.sql.Types} constants)
      * is a special type.
-     * 
+     *
      * @param jdbcTypeCode The type code
      * @return <code>true</code> if the type is a special one
      */
     public static boolean isSpecialType(int jdbcTypeCode)
     {
-        Set typesInCategory = (Set)_typesPerCategory.get(JdbcTypeCategoryEnum.SPECIAL);
+        Set<Integer> typesInCategory = _typesPerCategory.get(JdbcTypeCategoryEnum.SPECIAL);
 
-        return typesInCategory == null ? false : typesInCategory.contains(new Integer(jdbcTypeCode));
+        return typesInCategory == null ? false : typesInCategory.contains(Integer.valueOf(jdbcTypeCode));
     }
 }
---
+++ src/main/java/org/apache/ddlutils/util/Jdbc4Utils.java
@@ -0,0 +1,107 @@
+package org.apache.ddlutils.util;
+
+/*
+ * Copyright 1999-2006 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.sql.Types;
+
+import org.apache.ddlutils.model.TypeMap;
+
+/**
+ * Little helper class providing functions for dealing with the newer JDBC 4.0 functionality.
+ *
+ */
+public abstract class Jdbc4Utils
+{
+    /**
+     * Determines whether the system supports the JDBC 4.0 Types
+     */
+    public static boolean supportsJdbc40Types()
+    {
+        try {
+            return (Types.class.getField(TypeMap.NCHAR) != null) &&
+                (Types.class.getField(TypeMap.NVARCHAR) != null) &&
+                (Types.class.getField(TypeMap.NCLOB) != null) &&
+                (Types.class.getField(TypeMap.LONGNVARCHAR) != null);
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    /**
+     * Determines the type code for the NCHAR JDBC type.
+     *
+     * @return The type code
+     * @throws UnsupportedOperationException If the NCHAR type is not supported
+     */
+    public static int determineNCharTypeCode()
+        throws UnsupportedOperationException
+    {
+        try {
+            return Types.class.getField(TypeMap.NCHAR).getInt(null);
+        } catch (Exception ex) {
+            throw new UnsupportedOperationException("The jdbc type NCHAR is not supported");
+        }
+    }
+
+    /**
+     * Determines the type code for the NVARCHAR JDBC type.
+     *
+     * @return The type code
+     * @throws UnsupportedOperationException If the NVARCHAR type is not supported
+     */
+    public static int determineNVarcharTypeCode()
+        throws UnsupportedOperationException
+    {
+        try {
+            return Types.class.getField(TypeMap.NVARCHAR).getInt(null);
+        } catch (Exception ex) {
+            throw new UnsupportedOperationException("The jdbc type NVARCHAR is not supported");
+        }
+    }
+
+    /**
+     * Determines the type code for the LONGNVARCHAR JDBC type.
+     *
+     * @return The type code
+     * @throws UnsupportedOperationException If the LONGNVARCHAR type is not supported
+     */
+    public static int determineLongNVarcharTypeCode()
+        throws UnsupportedOperationException
+    {
+        try {
+            return Types.class.getField(TypeMap.LONGNVARCHAR).getInt(null);
+        } catch (Exception ex) {
+            throw new UnsupportedOperationException("The jdbc type LONGNVARCHAR is not supported");
+        }
+    }
+
+    /**
+     * Determines the type code for the NCLOB JDBC type.
+     *
+     * @return The type code
+     * @throws UnsupportedOperationException If the NCLOB type is not supported
+     */
+    public static int determineNClobTypeCode()
+        throws UnsupportedOperationException
+    {
+        try {
+            return Types.class.getField(TypeMap.NCLOB).getInt(null);
+        } catch (Exception ex) {
+            throw new UnsupportedOperationException("The jdbc type NCLOB is not supported");
+        }
+    }
+}
